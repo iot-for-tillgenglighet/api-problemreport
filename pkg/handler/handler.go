5c6b9cd313cbd483db68b5892253ef0e6147dddd
Sofia Styf <hsestyf@hotmail.co.uk>
2020-11-09 12:00:52 +0100

    refactored handler, moved context into own folder under fiware

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"compress/flate"
 	"net/http"
 	"os"
 
@@ -9,8 +10,11 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	fiwarecontext "github.com/iot-for-tillgenglighet/api-problemreport/internal/pkg/fiware/context"
 	gql "github.com/iot-for-tillgenglighet/api-problemreport/internal/pkg/graphql"
 	"github.com/iot-for-tillgenglighet/api-problemreport/pkg/database"
+	"github.com/iot-for-tillgenglighet/ngsi-ld-golang/pkg/ngsi-ld"
 
 	"github.com/rs/cors"
 
@@ -22,11 +26,7 @@ type RequestRouter struct {
 	impl *chi.Mux
 }
 
-//Router sets up and serves http router.
-func CreateRouterAndStartServing(db database.Datastore) *RequestRouter {
-
-	router := &RequestRouter{impl: chi.NewRouter()}
-
+func (router *RequestRouter) addGraphQLHandlers(db database.Datastore) {
 	gqlServer := handler.New(gql.NewExecutableSchema(gql.Config{Resolvers: &gql.Resolver{}}))
 	gqlServer.AddTransport(&transport.POST{})
 	gqlServer.Use(extension.Introspection{})
@@ -35,6 +35,50 @@ func CreateRouterAndStartServing(db database.Datastore) *RequestRouter {
 
 	router.impl.Handle("/api/graphql/playground", playground.Handler("GraphQL playground", "/api/graphql"))
 	router.impl.Handle("/api/graphql", gqlServer)
+}
+
+func (router *RequestRouter) addNGSIHandlers(contextRegistry ngsi.ContextRegistry) {
+	router.Get("/ngsi-ld/v1/entities", ngsi.NewQueryEntitiesHandler(contextRegistry))
+}
+
+func (router *RequestRouter) Get(pattern string, handlerFn http.HandlerFunc) {
+	router.impl.Get(pattern, handlerFn)
+}
+
+func newRequestRouter() *RequestRouter {
+	router := &RequestRouter{impl: chi.NewRouter()}
+
+	router.impl.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		Debug:            false,
+	}).Handler)
+
+	// Enable gzip compression for ngsi-ld responses
+	compressor := middleware.NewCompressor(flate.DefaultCompression, "application/json", "application/ld+json")
+	router.impl.Use(compressor.Handler)
+	router.impl.Use(middleware.Logger)
+
+	return router
+}
+
+func createRequestRouter(contextRegistry ngsi.ContextRegistry, db database.Datastore) *RequestRouter {
+	router := newRequestRouter()
+
+	router.addGraphQLHandlers(db)
+	router.addNGSIHandlers(contextRegistry)
+
+	return router
+}
+
+//CreateRouterAndStartServing creates a request router, registers all handlers and starts serving requests.
+func CreateRouterAndStartServing(db database.Datastore) {
+
+	contextRegistry := ngsi.NewContextRegistry()
+	ctxSource := fiwarecontext.CreateSource(db)
+	contextRegistry.Register(ctxSource)
+
+	router := createRequestRouter(contextRegistry, db)
 
 	port := os.Getenv("PROBLEMREPORT_API_PORT")
 	if port == "" {
@@ -43,9 +87,5 @@ func CreateRouterAndStartServing(db database.Datastore) *RequestRouter {
 
 	log.Printf("Starting api-problemreport on port %s.\n", port)
 
-	c := cors.Default().Handler(router.impl)
-
-	log.Fatal(http.ListenAndServe(":"+port, c))
-
-	return router
+	log.Fatal(http.ListenAndServe(":"+port, router.impl))
 }
